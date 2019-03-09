2ee8ad883a4e6aa65dd4a21dae0090b86f67ea87
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-03-09 11:18:46 +0800

    -

--- examples/simple/src/index.js
+++ examples/simple/src/index.js
@@ -5,7 +5,7 @@ const app = () => {
   showInfo([APP_NAME, APP_DESC].join(' - '), 'h1')
   showInfo('app is running...')
   import('./lib/remote').then(m => {
-    console.log(m)
+    m.test()
   })
 }
 
