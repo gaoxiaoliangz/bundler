163977b6f628b706cd24678dd51a2e5006117124
Liang <gaoxiaoliangz@gmail.com>
2019-03-14 12:27:07 +0800

    add tests

--- examples/simple/src/tests.js
+++ examples/simple/src/tests.js
@@ -1,10 +1,12 @@
 import * as testExport from './utils/testExport'
+import testExport2 from './utils/testExport2'
 import log from './utils/log'
 
 // TODO: 如果写成 export const runTests = () => { import(...).then(...) },
 // dynamic import 不会被处理
 const runTests = () => {
   log('testExport', testExport)
+  log('testExport2', testExport2)
 
   // test dynamic import
   // import(log).then(a => {})
---
+++ examples/simple/src/utils/testExport2.js
@@ -0,0 +1,12 @@
+const arr = [
+  {
+    test: 2,
+  },
+]
+
+export default [
+  {
+    test: 1,
+  },
+  ...arr,
+]
